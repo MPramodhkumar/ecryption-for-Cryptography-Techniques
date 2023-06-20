5d53bb14afde9cbb67d6dcadcd736b36787d8648
Pramodhkumar <114848124+MPramodhkumar@users.noreply.github.com>
2023-06-20 23:39:48 +0530

    Update OneTimePad.py

--- OneTimePad.py
+++ OneTimePad.py
@@ -1,27 +1,50 @@
-#one pad time 
+def otp_encrypt(plaintext, key):
+    ciphertext = ""
+    plaintext = plaintext.lower()
+    key = key.lower()
+    for i in range(len(plaintext)):
+        char = plaintext[i]
+        if char.isalpha():
+            p = alp.index(char)
+            # k = ord(key[i % len(key)]) - ord("A")
+            k = alp.index(key[i % len(key)])
+            c = (p + k) % 26
+            # ciphertext += chr(c + ord("A"))
+            ciphertext += alp[c]
+        else:
+            ciphertext += char
+    return ciphertext
 
 
+def otp_decrypt(ciphertext, key):
+    plaintext = ""
+    ciphertext = ciphertext.lower()
+    key = key.lower()
 
-def encrypt_char(plain_char, key_char):
-    return chr(((ord(plain_char) - ord('a') + ord(key_char) - ord('a')) % 26) + ord('a'))
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+        if char.isalpha():
+            # c = ord(char) - ord("A")
+            c = alp.index(char)
+            k = alp.index(key[i % len(key)])
+            p = (c - k) % 26
+            plaintext += alp[p]
+        else:
+            plaintext += char
+    return plaintext
 
-def decrypt_char(cipher_char, key_char):
-    return chr(((ord(cipher_char) - ord('a') - (ord(key_char) - ord('a')) + 26) % 26) + ord('a'))
-
-def encrypt(plaintext, key):
-    return "".join(encrypt_char(plain_char, key_char) for plain_char, key_char in zip(plaintext, key))
-
-def decrypt(ciphertext, key):
-    return "".join(decrypt_char(cipher_char, key_char) for cipher_char, key_char in zip(ciphertext, key))
 
 def xor_binary_strings(a, b):
-    return ''.join(str(int(x) ^ int(y)) for x, y in zip(a, b))
+    return "".join(str(int(x) ^ int(y)) for x, y in zip(a, b))
+
 
 def pad_key(plaintext, key):
     while len(key) < len(plaintext):
         key += key
-    return key[:len(plaintext)]
+    return key[: len(plaintext)]
 
+
+alp = list("abcdefghijklmnopqrstuvwxyz")
 while True:
     print("Choose Mode:")
     print("1. Manual (using formula (plaintext + key) % 26)")
@@ -29,42 +52,37 @@ while True:
     print("3. Exit")
 
     mode = input("Enter your mode: ")
-
-    if mode == '1':
+    if mode == "1":
         while True:
             print("---Manual (using formula (plaintext + key) % 26)---")
             print("Options:")
             print("1. Encrypt")
             print("2. Decrypt")
             print("3. Back to Mode Selection")
-            
-            choice = input("Enter your choice: ")
-            
-            if choice == '1':
-                plaintext = input("Enter plaintext: ")
-                key = input("Enter key: ")
-                ciphertext = encrypt(plaintext, key)
-                print("Ciphertext:", ciphertext)
-            elif choice == '2':
-                ciphertext = input("Enter ciphertext: ")
-                key = input("Enter key: ")
-                decrypted_text = decrypt(ciphertext, key)
-                print("Decrypted text:", decrypted_text)
-            elif choice == '3':
+            choice = input("Enter your choice:")
+            if choice == "1":
+                plaintext = input("Enter text:")
+                key = input("Enter key:")
+                print("Encrypted_text:", otp_encrypt(plaintext, key))
+            elif choice == "2":
+                ciphertext = input("Enter text:")
+                key = input("Enter key:")
+                print("Decrypted_text:", otp_decrypt(ciphertext, key))
+            elif choice == "3":
                 break
             else:
-                print("Invalid choice, please try again.")
-    elif mode == '2':
+                print("choose valid choice!")
+    elif mode == "2":
         while True:
             print("---Binary (using XOR operation)---")
             print("Options:")
             print("1. Encrypt")
             print("2. Decrypt")
             print("3. Back to Mode Selection")
-            
+
             choice = input("Enter your choice: ")
-            
-            if choice == '1':
+
+            if choice == "1":
                 plaintext = input("Enter plaintext in binary: ")
                 key = input("Enter key in binary: ")
 
@@ -72,19 +90,17 @@ while True:
                 ciphertext = xor_binary_strings(plaintext, padded_key)
 
                 print("Ciphertext:", ciphertext)
-            elif choice == '2':
+            elif choice == "2":
                 ciphertext = input("Enter ciphertext in binary: ")
                 key = input("Enter key in binary: ")
-
-                padded_key = pad_key(ciphertext, key)
                 decrypted_text = xor_binary_strings(ciphertext, padded_key)
 
                 print("Decrypted text:", decrypted_text)
-            elif choice == '3':
+            elif choice == "3":
                 break
             else:
                 print("Invalid choice, please try again.")
-    elif mode == '3':
+    elif mode == "3":
         break
     else:
         print("Invalid mode, please try again.")
