acc5f9be7abd7eff37b4962ab7e308df32e7f7a6
Pramodhkumar <114848124+MPramodhkumar@users.noreply.github.com>
2023-06-21 09:14:16 +0530

    Update HillCipher.py

--- HillCipher.py
+++ HillCipher.py
@@ -1,13 +1,19 @@
 import numpy as np
 import math
+
+
 def text_to_matrix(text, size):
+    alp = list("abcdefghijklmnopqrstuvwxyz")
     text = text.upper()
-    text = [ord(char) - 65 for char in text]
+    text = [alp.index(char.lower()) for char in text if char.lower() in alp]
+
     return np.array(text).reshape(size, size)
 
+
 def matrix_to_text(matrix):
-    text = ''.join([chr(num + 65) for num in matrix.flatten()])
+    text = "".join([chr(num + 65) for num in matrix.flatten()])
     return text
+
 
 def hill_cipher_encrypt(plaintext, key_message):
     key_size = 2
@@ -18,24 +24,28 @@ def hill_cipher_encrypt(plaintext, key_message):
 
     plaintext = plaintext.upper()
     if len(plaintext) % 2 == 1:
-        plaintext += 'X'
+        plaintext += "X"
     plaintext_values = [ord(char) - 65 for char in plaintext]
-    plaintext_blocks = [plaintext_values[i:i + key_size] for i in range(0, len(plaintext_values), key_size)]
+    plaintext_blocks = [
+        plaintext_values[i : i + key_size]
+        for i in range(0, len(plaintext_values), key_size)
+    ]
 
-    ciphertext = ''
+    ciphertext = ""
     for block in plaintext_blocks:
         encrypted_block = np.dot(key_matrix, block) % 26
         ciphertext += matrix_to_text(encrypted_block)
-    ciphertext = ciphertext.rstrip('X')
+    ciphertext = ciphertext.rstrip("X")
     return ciphertext
-    
+
 
 def mod_inv(a, m):
     g = math.gcd(a, m)
     if g != 1:
-        raise ValueError("Modular inverse does not exist")  
+        raise ValueError("Modular inverse does not exist")
     else:
         return pow(a, -1, m)
+
 
 def hill_cipher_decrypt(ciphertext, key_message):
     key_size = 2
@@ -44,47 +54,53 @@ def hill_cipher_decrypt(ciphertext, key_message):
     try:
         det_inv = mod_inv(det, 26)
     except ValueError:
-        print("Invalid key for decryption. Choose another key or modify the existing one.")
+        print(
+            "Invalid key for decryption. Choose another key or modify the existing one."
+        )
         return
-    
-    adj_matrix = np.array([[key_matrix[1, 1], -key_matrix[0, 1]], [-key_matrix[1, 0], key_matrix[0, 0]]])
+
+    adj_matrix = np.array(
+        [[key_matrix[1, 1], -key_matrix[0, 1]], [-key_matrix[1, 0], key_matrix[0, 0]]]
+    )
     key_matrix_inv = np.round(det_inv * adj_matrix) % 26
 
     ciphertext = ciphertext.upper()
     if len(ciphertext) % 2 == 1:
-        ciphertext += 'X'
+        ciphertext += "X"
     ciphertext_values = [ord(char) - 65 for char in ciphertext]
-    ciphertext_blocks = [ciphertext_values[i:i + key_size] for i in range(0, len(ciphertext_values), key_size)]
+    ciphertext_blocks = [
+        ciphertext_values[i : i + key_size]
+        for i in range(0, len(ciphertext_values), key_size)
+    ]
 
-    plaintext = ''
+    plaintext = ""
     for block in ciphertext_blocks:
         decrypted_block = np.dot(key_matrix_inv, block) % 26
         plaintext += matrix_to_text(decrypted_block)
-    plaintext = plaintext.rstrip('X')
+    plaintext = plaintext.rstrip("X")
     return plaintext
 
 
 while True:
     print("\n**HILL CIPHER**")
     print("\n1.Encrypt\n2.Decrypt\n3.Exit\n")
-    mode=input("Choose choice:")
-    if mode =='1' :
+    mode = input("Choose choice:")
+    if mode == "1":
         plaintext = input("Enter Plaintext:")
         key_message = input("Enter key in message format(4 Letters):")
-        if len(key_message)==4:
+        if len(key_message) == 4:
             ciphertext = hill_cipher_encrypt(plaintext, key_message)
             print()
             print("Cipher text :", ciphertext.lower())
         else:
             print("Enter valid key!")
-    elif mode == '2':
-          ciphertext = input("Enter Ciphertext:")
-          key_message = input("Enter key in message format(4 Letters):")
-          decrypted_text = hill_cipher_decrypt(ciphertext, key_message)
-          print()
-          print("Decrypted text:", decrypted_text)
-    elif mode == '3':
+    elif mode == "2":
+        ciphertext = input("Enter Ciphertext:")
+        key_message = input("Enter key in message format(4 Letters):")
+        decrypted_text = hill_cipher_decrypt(ciphertext, key_message)
+        print()
+        print("Decrypted text:", decrypted_text)
+    elif mode == "3":
         break
     else:
         print("Choose correct option!")
-    
