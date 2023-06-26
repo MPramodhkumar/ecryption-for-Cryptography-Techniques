cb6071d2f37fd69e15e6d5355656add9f86bdc40
Pramodhkumar <114848124+MPramodhkumar@users.noreply.github.com>
2023-06-26 19:05:46 +0530

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,80 +1,90 @@
-while True:
-    print("\n1.Manual\n2.Mathamatical\n3.Exit\n")
-    mode=input("Choose choice:")
-    print("\n")
-    if mode == '1':
-        letters='abcdefghijklmnopqrstuvwxyz'
-        num_letters=len(letters)
-        def encrypt_decrypt(text, mode, key):
-            result=''
-            if mode=='d':
-                key=-key
+def encrypt_decrypt(text, choice, key):
+    result = ""
+    if choice == "2":
+        key = -key
 
-            for letter in text:
-                letter=letter.lower()
-                if not letter==' ':
-                    index=letters.find(letter)
-                    if index==-1:
-                        result+=letter
-                    else:
-                        new_index=index+key
-                        if new_index>=num_letters:
-                            new_index-=num_letters
-                        elif new_index<0:
-                            new_index+=num_letters
-                        result+=letters[new_index]
-            return result
+    for letter in text:
+        letter = letter.lower()
+        if not letter == " ":
+            index = letters.find(letter)
+            if index == -1:
+                result += letter
+            else:
+                new_index = index + key
+                if new_index >= num_letters:
+                    new_index -= num_letters
+                elif new_index < 0:
+                    new_index += num_letters
+                result += letters[new_index]
+    return result
+
+
+def mathmatical_encrypt(plaintext, key):
+    ciphertext = ""
+    plaintext = plaintext.lower()
+    for i in range(len(plaintext)):
+        char = plaintext[i]
+        if char.isalpha():
+            p = letters.index(char)
+            c = (p + key) % 26
+            ciphertext += letters[c]
+        else:
+            ciphertext += char
+    return ciphertext
+
+
+def mathmatical_decrypt(ciphertext, key):
+    plaintext = ""
+    ciphertext = ciphertext.lower()
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+        if char.isalpha():
+            c = letters.index(char)
+            p = (c - key) % 26
+            plaintext += letters[p]
+        else:
+            plaintext += char
+    return plaintext
+
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+num_letters = len(letters)
+while True:
+    mode = input("1.Manual 2.Mathamatical 3.Exit: ")
+    if mode == "1":
         while True:
-            user_input=input("1.encrypt\n2.decrypt\n3.exit : ")
-            print()
-            if user_input=='1':
-                print("encryption mode selected:")
-                key=int(input("enter shiftkey b/w 1 to 26: "))
-                text=input("enter text to encrypt: ")
-                cipher=encrypt_decrypt(text,user_input,key)
-                print(f'ciphertext: {cipher}')
-            elif user_input=='2':
-                print("decryption mode selected:")
-                key=int(input("enter shiftkey b/w 1 to 26: "))
-                text=input("enter text to decrypt: ")
-                plain=encrypt_decrypt(text,user_input,key)
-                print(f'plaintext: {plain.upper()}')
-            elif user_input=="3":
+            choice = input("1.encrypt 2.decrypt 3.exit : ")
+            if choice == "1" or choice == "2":
+                plaintext = input("enter text to encrypt: ")
+                key = int(input("enter shiftkey b/w 1 to 26: "))
+                res = encrypt_decrypt(plaintext, choice, key)
+                if choice == "1":
+                    print("ciphertext:", res)
+                elif choice == "2":
+                    print("plaintext:", res)
+
+            elif choice == "3":
                 break
             else:
                 print("enter valid choice")
-    elif mode == '2':
-        def ceaser_cipher(text, key, type):
-            alp = list("abcdefghijklmnopqrstuvwxyz")
-            ans = ""
-            for i in text:
-                if i == " ": ans += " "
-                elif i.isupper():
-                    if type == 1:
-                        ans += alp[(alp.index(i.lower()) + key) % 26].upper()
-                    elif type == 2:
-                        ans += alp[(alp.index(i.lower()) - key) % 26].upper()
-                else:
-                    if type == 1:
-                        ans += alp[(alp.index(i) + key) % 26]
-                    elif type == 2:
-                        ans += alp[(alp.index(i) - key) % 26]
-            print(ans)
+    elif mode == "2":
         while True:
-            text = input("Enter the text: ")
-            key = int(input("Enter the shift key: "))
-            print("Encryption : 1")
-            print("Decryption : 2")
-            print("Exit : 3")
-            type = int(input("> "))
-            if type != 1 and type != 2 and type!=3:
-                print("Invalid Input")
-                continue
-            ceaser_cipher(text, key, type)
-    elif mode=="3":
-        exit()
+            choice = input("1.encrypt 2.decrypt 3.exit : ")
+            if choice == "1":
+                plaintext = input("enter text to encrypt: ")
+                key = int(input("enter shiftkey b/w 1 to 26: "))
+                ciphertext = mathmatical_encrypt(plaintext, key)
+                print("Ciphertext:", ciphertext)
+            elif choice == "2":
+                ciphertext = input("enter text to encrypt: ")
+                key = int(input("enter shiftkey b/w 1 to 26: "))
+                plaintext = mathmatical_decrypt(ciphertext, key)
+                print("plaintext:", plaintext)
+            elif choice == "3":
+                break
+            else:
+                print("enter valid choice")
+    elif mode == "3":
+        break
     else:
-        print("choose correct choice")
-        print("\n")
-        continue
-
+        print("Inavalid option!")
